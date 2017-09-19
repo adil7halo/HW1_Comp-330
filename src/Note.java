1d721be5e8b48e7e914beb22c8f30bd646ad74fa
Adil Ahmad <aahmad8@luc.edu>
2017-09-19 18:30:55 +0000

    search for keywords/mentions in a note

--- src/Note.java
+++ src/Note.java
@@ -7,8 +7,8 @@ import java.util.regex.*;
  *  Note class
  */
 public class Note {
-	
-    private String name;
+ 
+  private String name;
   private String path;
   private File file;
   private ArrayList<String> mentions;
@@ -28,7 +28,6 @@ public class Note {
     parse();
   }
   
-}
   // Method to parse Note file
   private void parse() {
     Pattern mentionPattern = Pattern.compile("@\\w+");
@@ -67,3 +66,61 @@ public class Note {
     }
   }
   
+  // Get name of Note
+  public String getName() {
+    return name;
+  }
+  
+  // Determine if Note has mentions
+  public boolean hasMentions() {
+    return mentions.size() > 0;
+  }
+  
+  // Display Note's mentions
+  public void displayMentions() {
+    System.out.println("Note: " + name);
+    if (!hasMentions()) {
+      System.out.println("Mentions: None");
+    } else {
+      String result = "Mentions: ";
+      for (String mention: mentions) {
+        result += mention + " ";
+      }
+      System.out.println(result);
+    }
+  }
+  
+  // Determine if Note has keywords
+  public boolean hasKeywords() {
+    return keywords.size() > 0;
+  }
+  
+  // Display keywords of Note
+  public void displayKeywords() {
+    System.out.println("Note: " + name);
+    if (!hasKeywords()) {
+      System.out.println("Keywords: None");
+    } else {
+      String result = "Keywords: ";
+      for (String keyword: keywords) {
+        result += keyword + " ";
+      }
+      System.out.println(result);
+    }
+  }
+  
+  // Search Note for keyword/mention term
+  public boolean search(String term) {
+    for (String mention: mentions) {
+      if (mention.contains(term)) {
+        return true;
+      }
+    }
+    for (String keyword: keywords) {
+      if (keyword.contains(term)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
