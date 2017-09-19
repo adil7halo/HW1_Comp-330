541b34393e985a6a69ffabb43aff6b8ad5dca491
Adil Ahmad <aahmad8@luc.edu>
2017-09-19 03:21:31 +0000

    Generate reports of mentions & Keywords

--- src/NoteSystem.java
+++ src/NoteSystem.java
@@ -32,4 +32,35 @@ public class NoteSystem {
     }
   }
   
+  // Generate report of notes organized by mentions
+  public void report2() {
+    System.out.println("Notes organized by mentions:");
+    for (String noteName: notes.keySet()) {
+      Note note = notes.get(noteName);
+      if (note.hasMentions()) {
+        note.displayMentions();
+      }
+    }
+  }
   
+  // Generate report of notes containing one or more keywords
+  public void report3() {
+    System.out.println("Notes containing one or more keywords:");
+    for (String noteName: notes.keySet()) {
+      Note note = notes.get(noteName);
+      if (note.hasKeywords()) {
+        System.out.println(noteName);
+      }
+    }
+  }
+  
+  // Generate report of notes organized by keywords
+  public void report4() {
+    System.out.println("Notes organized by keywords:");
+    for (String noteName: notes.keySet()) {
+      Note note = notes.get(noteName);
+      if (note.hasKeywords()) {
+        note.displayKeywords();
+      }
+    }
+  }
